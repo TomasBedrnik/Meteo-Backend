d5f7d54c90bd63b8886c5e4a077943993ba90c3a
Tomas <tomas.bedrnik@email.cz>
2016-05-26 23:08:50 +0200

    Change Cron Recommended Settings

--- getRadarImages.py
+++ getRadarImages.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #should be executed every 10 minutes with cron
-#2,12,22,32,42,52 * * * * /path/preloadImages.py
+#1,11,21,31,41,51 * * * * /path/getRadarImages.py
 import shutil
 import requests
 from datetime import datetime
@@ -23,13 +23,9 @@ if (os.path.isdir(imageDir) == False):
   makedirs(imageDir)
 
 now = datetime.utcnow()
-#now = datetime(2016, 5, 26, 18, 40, 00, 366086)
 minutes = int(now.strftime("%M"))//10*10;
 imageURLLightning = "http://radar.bourky.cz/data/celdn/pacz2gmaps.blesk."+now.strftime("%Y%m%d.%H")+str(minutes).zfill(2)+".png";
 imageURLRadar = "http://radar.bourky.cz/data/pacz2gmaps.z_max3d."+now.strftime("%Y%m%d.%H")+str(minutes).zfill(2)+".0.png";
-
-#print(imageURLBlesk);
-#print(imageURLRadar);
 
 response = requests.get(imageURLLightning, stream=True)
 with open(imageDir+"/lightning"+now.strftime("%Y%m%d.%H")+str(minutes).zfill(2)+".png", "wb") as out_file:
@@ -44,6 +40,6 @@ del response
 now = time.time()
 for f in os.listdir(imageDir):
   f = os.path.join(imageDir, f)
-  if os.stat(f).st_mtime < now - 5*60:
+  if os.stat(f).st_mtime < now - 2*60*60:
     if os.path.isfile(f):
       os.remove(os.path.join(imageDir, f))
