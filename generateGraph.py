309bcc76518fc88723077ecf99dfa78b438ec23b
Tomas <tomas.bedrnik@email.czcd>
2016-06-10 11:13:16 +0200

    Fix bug - no graph

--- generateGraph.py
+++ generateGraph.py
@@ -192,7 +192,7 @@ if os.path.isdir(graphDir):
     shutil.rmtree(graphDir)
 makedirs(graphDir)
 
-#fig.savefig(graphDir+'/graph1.svg', transparent=True,bbox_inches='tight')
+fig.savefig(graphDir+'/graph1.svg', transparent=True,bbox_inches='tight')
 #scrpipt = os.path.join(os.getcwd(), 'uploadToDrive.py')
 #string = scrpipt + " -replace graph1.svg graph "+graphDir+"/graph1.svg"
 #print(string)
