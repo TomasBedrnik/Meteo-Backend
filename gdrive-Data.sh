1e16c294e9043ddd68320b97327f03dd012082da
Tomas <tomas.bedrnik@email.cz>
2017-02-05 13:12:57 +0100

    Add bash scripts for uploading to Google Drive

---
+++ gdrive-Data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#date +"%H:%M:%S %d.%m.%Y" >> /tmp/log-drive-Data
+grive -s meteor-Data -p /home/beda/
---
+++ gdrive-Graph.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#date +"%H:%M:%S %d.%m.%Y" >> /tmp/log-drive-Graph
+grive -s meteor-Graph -p /home/beda/
